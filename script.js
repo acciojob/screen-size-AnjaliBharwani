5685a5323e18ee34c2e8bad6da62a1286a956a6e
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-09-22 20:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateSizeInfo(){
+window.addEventListener("resize", getscreensize);
+
+function getscreensize(){
+	let element = document.getElementById("sizeInfo");
+	let x = document.documentElement.clientWidth;
+	let y = document.documentElement.clientHeight;
+	element.innerHTML= `<h1>Width: ${x} and Height: ${y}</h1>`;
+}
