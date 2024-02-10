fa25181a1ac65d5c19fc75823583b962e253d826
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2024-02-10 16:55:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-//your JS code here. If required
- function updateWindowSize() {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
+//your JS code here. If required.
+let x = window.innerWidth;
+let y = window.innerHeight;
 
-      const sizeInfo = document.getElementById("sizeInfo");
-      const h1 = sizeInfo.querySelector("h1");
-
-      h1.textContent = `Width: ${width} Height: ${height}`;
-    }
-
-    // Initial update
-    updateWindowSize();
-
-    // Add a resize event listener to update the size on window resize
-    window.addEventListener("resize", updateWindowSize);
+ab.innerText = `Width: ${x} and Height: ${y}`;
+window.addEventListener('resize', () => {
+	let x = window.innerWidth;
+	let y = window.innerHeight;
+	
+	ab.innerText = `Width: ${x} and Height: ${y}`;
+})
